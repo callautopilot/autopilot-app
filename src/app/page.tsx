42d09fb92aa3b2b06e23bf7d9dd90a7de68d1940
Louis Rouffineau <lourou@users.noreply.github.com>
2023-12-20 09:20:28 +0100

    UI skeleton (#3)
    * Replace default vercel favicon with a mysterious black dot
    
    * Enforce dark mode color scheme
    
    * UI Skeleton
    
    * Wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,130 @@
 'use client'
 
-import { Box, Container, Stack, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  Text,
+  VStack,
+  useToast,
+  Heading,
+} from '@chakra-ui/react'
+import { FiMic, FiMicOff } from 'react-icons/fi';
 
 export default function Home() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const [recordedText, setRecordedText] = useState('');
+  const toast = useToast();
+
+  // Fake placholder text for dev
+  const fakeRecordedText = `This is an example of text being recorded from the microphone`;
+
+  useEffect(() => {
+    let timer: number | NodeJS.Timeout;
+    if (isRecording && recordedText !== fakeRecordedText && recordedText.length < fakeRecordedText.length) {
+      timer = setTimeout(() => {
+        setRecordedText(fakeRecordedText.slice(0, recordedText.length + 1));
+      }, 100);
+    }
+    return () => clearTimeout(timer);
+  }, [recordedText, isRecording]);
+
+  const renderIcon = () => {
+    return isRecording ? (hovered ? <FiMicOff /> : <FiMic />) : <FiMic />;
+  };
+
+  const handleRecordClick = () => {
+    setIsRecording(!isRecording);
+    if (!isRecording) {
+      setRecordedText('…');
+      toast({
+        title: 'Recording started',
+        status: 'info',
+        duration: 3000,
+        isClosable: false,
+        position: 'bottom',
+        variant: 'subtle',
+        size: 'sm',
+        containerStyle: {
+          background: 'transparent',
+          borderColor: 'blue.500',
+          color: 'blue.500',
+        },
+      });
+    } else {
+      toast({
+        title: 'Recording stopped',
+        status: 'success',
+        duration: 3000,
+        isClosable: false,
+        variant: 'subtle',
+        containerStyle: {
+          background: 'transparent',
+          borderColor: 'green.500',
+          color: 'green.500',
+        },
+      });
+    }
+  };
+
   return (
-    <Box as="section" py={{ base: '4', md: '8' }}>
-      <Container maxW="3xl">
-        <Box bg="bg.surface" boxShadow="sm" borderRadius="lg" p={{ base: '4', md: '6' }}>
-          <Stack spacing="5">
-            <Stack spacing="1">
-              <Text textStyle="lg" fontWeight="medium">
-                Autopilot
-              </Text>
-              <Text textStyle="sm" color="fg.muted">
-                This is the first Chakra UI element
+    <>
+      <Box bg="bg.surface">
+        <Container py={{ base: '16', md: '24' }}>
+          <Stack spacing={{ base: '12', md: '16' }} textAlign="center" align="center">
+            <Stack spacing={{ base: '4', md: '5' }}>
+              <Heading size={{ base: 'sm', md: 'md' }}>
+                🤖 Autopilot
+              </Heading>
+              <Text fontSize={{ base: 'lg', md: 'xl' }} color="fg.muted" maxW="3xl">
+                Because why not send someone else to your useless meetings
               </Text>
             </Stack>
           </Stack>
-        </Box>
+        </Container>
+      </Box>
+
+      <Container maxW="3xl">
+        <VStack py={{ base: '8', md: '12' }} align="center" justify="center">
+          <Button
+            leftIcon={renderIcon()}
+            colorScheme={isRecording ? 'red' : 'green'}
+            onClick={handleRecordClick}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            _hover={{
+              bg: isRecording ? 'red.500' : 'green.500',
+              transform: 'scale(1.04)',
+            }}
+            sx={{
+              animation: isRecording ? 'pulse 2s infinite' : 'none',
+            }}
+          >
+            {isRecording ? 'Recording...' : 'Start'}
+          </Button>
+        </VStack>
       </Container>
-    </Box>
+    
+      <Box as="section">
+        <Container maxW="3xl">
+          <Box bg="bg.surface" boxShadow="sm" borderRadius="lg" p={{ base: '4', md: '6' }}>
+            <VStack spacing={4}>
+              <Box textAlign="center">
+                <Text mb={2}>Recorded Text:</Text>
+                <Text>{recordedText}</Text>
+              </Box>
+              <Box textAlign="center">
+                <Text mb={2}>Potential Response:</Text>
+                <Text>(...)</Text>
+              </Box>
+            </VStack>
+          </Box>
+        </Container>
+      </Box>
+    
+    </>
   )
 }
