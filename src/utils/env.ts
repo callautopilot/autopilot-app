5f2ff269ffe855772351268c592828129e3eb9a9
Augustin Vignet <augustinvignet@gmail.com>
2024-04-23 22:17:19 +0200

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,40 +1,13 @@
-import z, { ZodError } from "zod";
 import dotenv from "dotenv";
 
 const envFilePath =
   process.env.NODE_ENV === "production" ? ".env" : ".env.local";
 dotenv.config({ path: envFilePath });
 
-const parsedEnvValidator = z.object({
-  // Server
-  NODE_ENV: z.string().optional().default("development"),
-  PORT: z.number().optional().default(3000),
-
-  // External services
-  //OPENAI_API_KEY: z.string().optional().default(""),
-  //DEEPGRAM_API_KEY: z.string().optional().default(""),
-  //ELEVEN_LABS_API_KEY: z.string().optional().default(""),
-  //ELEVEN_LABS_VOICE_ID: z.string().optional().default(""),
-});
-
-type ParsedEnv = z.infer<typeof parsedEnvValidator>;
-
-let parsedEnv: ParsedEnv;
-
-try {
-  parsedEnv = parsedEnvValidator.parse(process.env);
-} catch (error) {
-  if (error instanceof ZodError) {
-    console.error(`Environment validation error: ${error.flatten()}`);
-    process.exit(1);
-  }
-
-  throw error;
-}
-
 export const env = {
-  ...parsedEnv,
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
   get IS_DEV() {
-    return parsedEnv.NODE_ENV !== "production";
+    return process.env.NODE_ENV !== "production";
   },
 };
